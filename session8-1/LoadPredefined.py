e2a9fa882885831c1ce9a3ac1ddd08464715e9da
Sarat Duvvuri <sarat.duvvuri@disney.com>
2021-08-30 19:39:28 -0400

    all files

---
+++ session8-1/LoadPredefined.py
@@ -0,0 +1,32 @@
+# Import the Gensim library's downloader package which provides
+# access to predefined embeddings and other corpora.
+import gensim.downloader
+
+
+# Get a list of all the predefined things in the set.  This will
+# be a dict that breaks into two pieces, corpora which we can use
+# for text info, and models that we can access.
+gensim.downloader.info()
+
+# This lists the available models.
+gensim.downloader.info()['models'].keys()
+
+
+# We will use a smaller model that is trained on wikipedia.  You can
+# find the info here.
+gensim.downloader.info()['models']['glove-wiki-gigaword-50']
+
+# Now download the model itself.
+#  (NOTE: This may take a little while)
+GloveModel = gensim.downloader.load('glove-twitter-50')
+
+# And now we can do some simple synonym or similarity lookup
+# based upon the text.
+print(GloveModel.most_similar("flemish"))
+
+
+# And given a sequence of words print out the most similar for each.
+# This can be readily extended to any word list. 
+Words = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+for W in Words:
+    print(GloveModel.most_similar(W)[0][0])
--- session8-1/Slide16Example.py
+++ session8-1/Slide16Example.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import nltk
 from nltk.tokenize import TweetTokenizer 
+from nltk.corpus import stopwords
+import gensim
 
 
-nationalParks = ''
+nationalParks = 'https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States'
 cdc = ''
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
@@ -14,14 +16,49 @@ print(soup.get_text())
 PageText = soup.get_text()
 
 Req2 = requests.get(nationalParks)
-print(Req2.text[0:100])
-Req3 = requests.get(cdc)
-print(Req3.text[0:100])
+#print(Req2.text[0:100])
+soup = BeautifulSoup(Req2.text, features='lxml')
+parks = soup.get_text()
+#Req3 = requests.get(cdc)
+#print(Req3.text[0:100])
 
 
+nltk.download('punkt')
+nltk.download('stopwords')
 sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-Punctuation = sentence_detector.tokenize(PageText.strip())
+Punctuation = sentence_detector.tokenize(parks.strip())
 
-Twitter - TweetTokenizer()
-print(Twitter.tokenize(PageText))
-print(Punctuation)
+Twitter = TweetTokenizer()
+#print(Twitter.tokenize(parks))
+#print(Punctuation)
+useLess = [] 
+useLess.append('.')
+useLess.append(',')
+useLess.append("'")
+useLess.append('"')
+useLess.append('^')
+useLess.append('`')
+useLess.append('/')
+useLess.append(':')
+useLess.append('(')
+useLess.append(')')
+useLess.append('[')
+useLess.append(']')
+for word in stopwords.words('english'):
+  useLess.append(word)
+  print(word)
+
+
+Words = nltk.tokenize.word_tokenize(parks)
+dataset = []
+for d in Words:
+  if d not in useLess:
+    dataset.append([d])
+
+#print(dataset)  
+Model = gensim.models.Word2Vec(dataset, min_count=2)
+print('############################################################')
+print(Model.wv.key_to_index)
+
+vector= Model.wv['mountains']
+print(vector)
